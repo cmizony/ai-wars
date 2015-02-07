58e1b2144f060589c890ea969b924567cdd3237d
cmizony <cmizony@gmail.com>
2015-02-06 23:27:07 -0800

    :white_check_mark: Complete unit test for AI_player

--- php_simulator/tests/AI_playerTest.php
+++ php_simulator/tests/AI_playerTest.php
@@ -55,4 +55,57 @@ class AI_playerTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($mock_effects_json,$json_result['debuffs']);
 		$this->assertEquals($mock_effects_json,$json_result['cooldowns']);
 	}
+
+	public function testToString ()
+	{
+		$effect = $this->getMockBuilder('AI_effect')->getMock();
+		$effect->duration = 5;
+
+		$player = new AI_player ();
+		$player->id = 1;
+		$player->team = 2;
+		$player->energy = 100;
+		$player->life = 50;
+		$player->cast_bar = array($effect,$effect);
+		$player->buffs = array($effect,$effect);
+		$player->debuffs = array($effect,$effect);
+		$player->cooldowns = array($effect,$effect);
+
+		$string_result = (string)$player;
+		$expected_string = "P 1 2 50 100 B (0;5) (1;5) D (0;5) (1;5) C (0;5) (1;5) CD (0;5) (1;5)";
+		$this->assertEquals($expected_string,$string_result);
+	}
+
+	public function testClone ()
+	{
+		$effect = $this->getMockBuilder('AI_effect')->getMock();
+		$effect->duration = 5;
+
+		$player_model = new AI_player();
+		$player_model->id = 1;
+		$player_model->team = 2;
+		$player_model->energy = 100;
+		$player_model->life = 50;
+		$player_model->cast_bar = array($effect,$effect);
+		$player_model->buffs = array($effect,$effect);
+		$player_model->debuffs = array($effect,$effect);
+		$player_model->cooldowns = array($effect,$effect);
+
+		$player_cloned = clone $player_model;
+
+		$this->assertEquals(1,$player_cloned->id);
+		$this->assertEquals(2,$player_cloned->team);
+		$this->assertEquals(100,$player_cloned->energy);
+		$this->assertEquals(50,$player_cloned->life);
+
+		$player_model->cast_bar = NULL;
+		$player_model->buffs = NULL;
+		$player_model->debuffs = NULL;
+		$player_model->cooldowns = NULL;
+
+		$this->assertEquals(array($effect,$effect),$player_cloned->debuffs);
+		$this->assertEquals(array($effect,$effect),$player_cloned->buffs);
+		$this->assertEquals(array($effect,$effect),$player_cloned->debuffs);
+		$this->assertEquals(array($effect,$effect),$player_cloned->cooldowns);
+	}
 }
