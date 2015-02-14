238f7e5e19ce8b77c062f7575c030aa13c73b1ba
cmizony <cmizony@gmail.com>
2015-02-14 12:52:12 -0800

    :white_check_mark: Add tests for some spells effect

--- php_simulator/tests/AI_effectTest.php
+++ php_simulator/tests/AI_effectTest.php
@@ -44,6 +44,59 @@ class AI_effectTest extends PHPUnit_Framework_TestCase
 	public function testCastEffects ()
 	{
 
+		$list_spells = array(
+			AI_wars\AI_Spell::LASER_SHOT => "offensive",
+			AI_wars\AI_Spell::REPARING => "defensive",
+			AI_wars\AI_Spell::ELECTROMAGNETIC_SHOCK => "neutral",
+		);
+
+		foreach ($list_spells as $spell_id => $spell_type)
+		{
+			$player_source = $this->getMockBuilder('AI_player')->getMock();
+			$player_source->debuff = array();
+			$player_source->cooldowns = array();
+
+			$player_target = $this->getMockBuilder('AI_player')->getMock();
+			$player_target->life =100;
+			$player_target->debuff = array();
+			$player_target->cooldowns = array();
+			$player_target->cast_bar = array(
+				$this->getMockBuilder('AI_Effect')->getMock()
+			);
+
+			$spell = $this->getMockBuilder('AI_spell')->getMock();
+			$spell->id = $spell_id;
+			$spell->type = $spell_type;
+			$spell->duration = 3;
+			$spell->power = 10;
+			$spell->cooldown = 3;
+
+			$effect = new AI_wars\AI_Effect($spell);
+			$effect->p_source = $player_source;
+			$effect->p_target = $player_target;
+
+			$effect->do_cast();
+
+			switch ($spell_id)
+			{
+			case AI_wars\AI_Spell::LASER_SHOT:
+				$this->assertEquals(90,$player_target->life);
+				break;
+			case AI_wars\AI_Spell::REPARING:
+				$this->assertEquals(110,$player_target->life);
+				break;
+			case AI_wars\AI_Spell::ELECTROMAGNETIC_SHOCK:
+				$this->assertEmpty($player_target->cast_bar);
+				break;
+			case AI_wars\AI_Spell::ELECTROMAGNETIC_BLAST:
+				$this->assertEmpty($player_target->cast_bar);
+				$this->assertArrayHasKey(AI_wars\AI_Spell::ELECTROMAGNETIC_BLAST,$player_target->debuffs);
+				$this->assertEquals(3,$player_target->debuffs[AI_wars\AI_Spell::ELECTROMAGNETIC_BLAST]->duration);
+				break;
+
+
+			}
+		}
 	}
 
 	public function testDo_buff()
